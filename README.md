4a54c8d6ef03cbda4d81073f3ed08dd45c95a5a2
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2015-08-17 21:51:38 -0300

    Final post blog

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ casperjs test *.js
 * Tests case
 1. Verify http response code if equals to 200
 2. Verify if the title text is equals to *Fernando Fabricio*
-3. Verify if the element h1 text is equals to *Fernando Fabricio*
+3. Verify if the element h1 text is equals to *\nFernando Fabricio\n* (yes, the blogger add this break line in the title :T)
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-casper.test.begin('Testing index of blog.fernando.inf.br', 1, function suite(test){
+casper.test.begin('Testing index of blog.fernando.inf.br', 3, function (test){
     casper.start('http://blog.fabricio.inf.br', function(){
         test.assertHttpStatus(200);
+        test.assertTitle('Fernando Fabricio');
+        test.assertEquals(this.getHTML('#header-inner > div.titlewrapper > h1'), "\nFernando Fabricio\n");
     });
 
     casper.run(function(){
